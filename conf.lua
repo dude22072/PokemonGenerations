75004fec40e636c809aab85e4a92d1466acd3697
dude22072 <josephkeller1999@icloud.com>
2015-07-28 15:17:34 -0500

    Initial Commit

---
+++ conf.lua
@@ -0,0 +1,10 @@
+-- Configuration
+function love.conf(t)
+	t.title = "Pokemon Generations 0.0.0"
+	t.version = "0.9.2"
+	t.window.width = 640
+	t.window.height = 576
+
+	-- For Windows debugging
+	t.console = true
+end
